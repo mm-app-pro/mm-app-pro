758ca6996ecc8b3715de21a14eca01696693e84f
wanmei <wanmei@qq.com>
2018-03-24 17:49:22 +0800

    登录跳转

--- src/main/webapp/WEB-INF/js/login.js
+++ src/main/webapp/WEB-INF/js/login.js
@@ -30,7 +30,17 @@ $(function(){
             			console.log('登录失败，请稍后再试')
             		},
             		success:function(res){
-            			console.log(res)
+            			if(res.code==0){
+                            if(res.roleId==1){
+                                location.href = 'user/html';
+                            }else if(res.roleId==2){
+                                location.href = 'worker/html';
+                            }else{
+                                location.href = 'apply/html';
+                            }
+                        }else{                            
+                            layer.msg('登录失败！！');                        
+                        }
             		}
             })
         }
